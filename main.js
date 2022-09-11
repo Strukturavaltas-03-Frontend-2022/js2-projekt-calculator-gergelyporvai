463954c67643f50c2d4d63c2aee1e7a000114961
GergelyPorvai <porvai.gergely@gmail.com>
2022-09-12 01:14:31 +0200

    v1 css

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const display = document.querySelector(".p");
+const buttonAdd = document.querySelector("#button__add");
+const buttonSub = document.querySelector("#button__sub");
+const buttonMulti = document.querySelector("#button__multi");
+const buttonDiv = document.querySelector("#button__div");
+
+const button1 = document.querySelector("#button__1");
+const button2 = document.querySelector("#button__2");
+const button3 = document.querySelector("#button__3");
+const button4 = document.querySelector("#button__4");
+const button5 = document.querySelector("#button__5");
+const button6 = document.querySelector("#button__6");
+const button7 = document.querySelector("#button__7");
+const button8 = document.querySelector("#button__8");
+const button9 = document.querySelector("#button__9");
+const button0 = document.querySelector("#button__0");
+const buttonC = document.querySelector("#button__C");
+const buttonDot = document.querySelector("#button__dot");
